357def4aa096a2cd2d58773a9708133e3db06125
sting <bobadillasting@gmail.com>
2024-09-29 17:35:03 -0300

    Refactor PageMovies component to use SectionContainer for styling and SearchMovies for search functionality

--- src/pages/PageMovies.tsx
+++ src/pages/PageMovies.tsx
@@ -1,62 +1,39 @@
-import { Box, Typography } from "@mui/material";
-import { SearchForm } from "../components/SearchForm";
-import { TextInput } from "../components/TextInput";
-import { useSearchParams } from "react-router-dom";
-import { useCallback, useEffect, useState } from "react";
-import { useDebounce } from "../hooks/useDebounce";
+import { SectionContainer } from "../components/SectionContainer";
 import { useMovies } from "../hooks/useMovies";
 import { ListOfMovies } from "../components/ListOfMovies";
+import { SearchMovies } from "../components/SearchMovies";
+import { useSearchMovies } from "../hooks/useSearchMovies";
+import { useEffect } from "react";
 
 export function PageMovies() {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const query = searchParams.get('s') as string
 
-  const [inputValue, setInputValue] = useState<string>(query)
-  const { debounceValue } = useDebounce(inputValue, 500)
+  const { getAllMovies, movies, isLoading } = useMovies()
+  const { fieldText } = useSearchMovies()
 
-  const { movies, isLoading, getAllMovies, error } = useMovies()
-
-  const handleSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const queryField = formData.get('query') as string
-    getAllMovies(queryField)
-  }, [getAllMovies])
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newParamValue = event.currentTarget.value
-    setSearchParams({ s: newParamValue })
-    setInputValue(newParamValue)
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const movieName = formData.get('movieName') as string
+    getAllMovies(movieName)
   }
 
   useEffect(() => {
-    getAllMovies(debounceValue)
-  }, [debounceValue, getAllMovies])
+
+    if (fieldText !== '') {
+      getAllMovies(fieldText)
+    }
+
+  }, [fieldText])
 
   return (
     <>
-      <Box component={'section'} sx={{ padding: '20px 0 8px' }}>
-        <SearchForm isLoading={isLoading} onSubmit={handleSubmit}>
-          <TextInput
-            name="query"
-            id="query"
-            label="Search movie"
-            aria-label="Write the name of the movie you want to search"
-            value={inputValue}
-            onChange={handleChange}
-          />
-        </SearchForm>
-      </Box>
+      <SectionContainer>
+        <SearchMovies isLoading={isLoading} onSubmit={handleSubmit} />
+      </SectionContainer>
 
-      <Box component={'section'} sx={{ padding: '18px 0 8px' }}>
-        {!isLoading && error && (<Typography variant='h5' color='warning.main'
-          sx={{
-            textAlign: 'center'
-          }}>
-          {error}
-        </Typography>)}
-        {!isLoading && !error && <ListOfMovies movies={movies.movies} />}
-      </Box>
+      <SectionContainer>
+        <ListOfMovies movies={movies.movies} isLoading={isLoading} />
+      </SectionContainer>
 
       {/* Pagination */}
 

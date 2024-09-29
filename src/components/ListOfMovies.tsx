cd880d3aab704370e1a61b587a9c27f128a995e7
sting <bobadillasting@gmail.com>
2024-09-29 17:34:16 -0300

    Refactor ListOfMovies component to handle loading state

--- src/components/ListOfMovies.tsx
+++ src/components/ListOfMovies.tsx
@@ -1,18 +1,28 @@
 import { Grid2 } from "@mui/material";
-import { Movie } from "../types";
-import { NoMoviesResult } from "./NoMoviesResult";
 import { CardMovie } from "./CardMovie";
+import { Movie } from "../types";
+import { MovieSkeleton } from "./MovieSkeleton";
+import { NoMoviesResult } from "./NoMoviesResult";
 
-export function ListOfMovies({ movies }: { movies: Movie[] }) {
+export function ListOfMovies({ movies, isLoading = true }: { movies: Movie[], isLoading?: boolean }) {
 
-  if (movies.length === 0) {
+  if (isLoading) {
+    return (
+      <Grid2 container spacing={2} columns={{ xs: 2, sm: 4, lg: 5 }}>
+        <MovieSkeleton />
+        <MovieSkeleton />
+      </Grid2>
+    )
+  }
+
+  if (!isLoading && movies.length === 0) {
     return (
       <NoMoviesResult />
     )
   }
 
   return (
-    <Grid2 container spacing={2} columns={{ xs: 2, sm: 5, lg: 5 }}>
+    <Grid2 container spacing={2} columns={{ xs: 2, sm: 4, lg: 5 }}>
       {
         movies.map(movie => (
           <CardMovie key={movie.id} movie={movie} />

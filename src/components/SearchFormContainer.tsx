74e8a9243b0effeac09c80e484d54a482afd0763
sting <bobadillasting@gmail.com>
2024-09-29 17:34:38 -0300

    Refactor SearchFormContainer component to add styling

---
+++ src/components/SearchFormContainer.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+
+export const SearchFormContainer = styled('form')(() => ({
+  display: 'flex',
+  gap: '12px',
+  alignItems: 'start',
+  width: '100%',
+  maxWidth: '600px',
+  marginInline: 'auto'
+}))
